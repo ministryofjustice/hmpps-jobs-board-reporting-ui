dc3df235869e56b12c807ad5bfd4331606d98eed
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2024-10-18 14:03:56 +0100

    ESWE-1100_prepare_project_for_development Added common utilities, config entries

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -11,6 +11,7 @@ export default function nunjucksSetup(app: express.Express): void {
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
+  app.locals.dpsHomeUrl = config.dpsHomeUrl
   app.locals.applicationName = 'Hmpps Jobs Board Reporting Ui'
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
