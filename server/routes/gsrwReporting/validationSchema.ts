55e440e0955a87cb5a0e77e696b3fc3a5479badd
Mary Ford <mary.ford@justice.gov.uk>
2025-11-28 12:38:23 +0000

    ESWE-1517: Update validation on date picker (#46)
    - Adds inline errors to the date pickers - agreed with UCD that we will
    have one error across both date pickers, rather than separate errors for
    each date picker, as this looks better and is consistent with other
    services.
    - Adds 'Error: ' to the page title when an error is displayed on the
    page
    - Updates error messaging content to be consistent throughout, removing
    the use of quotation marks, and putting the errors into an appropriate
    priority order.

--- server/routes/gsrwReporting/validationSchema.ts
+++ server/routes/gsrwReporting/validationSchema.ts
@@ -1,6 +1,7 @@
 import { parse } from 'date-fns'
 import joi from 'joi'
 import type { ObjectSchema } from 'joi'
+import DateError from '../../utils/errors'
 
 export default function validationSchema(): ObjectSchema {
   return joi
@@ -8,21 +9,23 @@ export default function validationSchema(): ObjectSchema {
     .custom((obj, helper) => {
       const { dateFrom, dateTo } = obj
 
+      // No dates provided, skip validation
       if (!dateFrom && !dateTo) {
         return true
       }
 
+      // Check that both dates are provided
       if (dateFrom && !dateTo) {
-        return helper.error('any.dateToRequired', {
-          key: 'dateTo',
-          label: 'dateTo',
+        return helper.error('date.required', {
+          key: 'dates',
+          label: 'dates',
         })
       }
 
       if (!dateFrom && dateTo) {
-        return helper.error('any.dateFromRequired', {
-          key: 'dateFrom',
-          label: 'dateFrom',
+        return helper.error('date.required', {
+          key: 'dates',
+          label: 'dates',
         })
       }
 
@@ -33,64 +36,63 @@ export default function validationSchema(): ObjectSchema {
 
       if (Number.isNaN(dateFromObj.getTime())) {
         return helper.error('date.base', {
-          key: 'dateFrom',
-          label: 'dateFrom',
+          key: 'dates',
+          label: 'dates',
         })
       }
 
       if (Number.isNaN(dateToObj.getTime())) {
         return helper.error('date.base', {
-          key: 'dateTo',
-          label: 'dateTo',
-        })
-      }
-
-      // Check that no future dates are allowed
-      if (dateFromObj > now) {
-        return helper.error('date.future', {
-          key: 'dateFrom',
-          label: 'dateFrom',
-        })
-      }
-
-      if (dateToObj > now) {
-        return helper.error('date.future', {
-          key: 'dateTo',
-          label: 'dateTo',
+          key: 'dates',
+          label: 'dates',
         })
       }
 
       // Check dates are on or after 1 August 2025
       if (dateFromObj <= new Date('2025-07-31')) {
         return helper.error('date.past', {
-          key: 'dateFrom',
-          label: 'dateFrom',
+          key: 'dates',
+          label: 'dates',
         })
       }
 
       if (dateToObj <= new Date('2025-07-31')) {
         return helper.error('date.past', {
-          key: 'dateTo',
-          label: 'dateTo',
+          key: 'dates',
+          label: 'dates',
+        })
+      }
+
+      // Check that no future dates are allowed
+      if (dateFromObj > now) {
+        return helper.error('date.future', {
+          key: 'dates',
+          label: 'dates',
+        })
+      }
+
+      if (dateToObj > now) {
+        return helper.error('date.future', {
+          key: 'dates',
+          label: 'dates',
         })
       }
 
       // Check if dateTo is after dateFrom
       if (dateToObj < dateFromObj) {
         return helper.error('date.order', {
-          key: 'dateTo',
-          label: 'dateTo',
+          key: 'dates',
+          label: 'dates',
         })
       }
 
       return true
     })
     .messages({
-      'any.dateToRequired': 'Enter or select a `latest` date',
-      'any.dateFromRequired': 'Enter or select a `earliest` date',
-      'date.base': 'Enter the date in the correct format',
-      'date.order': "The `latest` date must be after the 'earliest' date",
-      'date.future': 'Dates must not be in the future',
-      'date.past': 'Dates must be on or after 1 August 2025',
+      'date.required': DateError.DATE_REQUIRED,
+      'date.base': DateError.DATE_FORMAT,
+      'date.past': DateError.DATE_PAST,
+      'date.future': DateError.DATE_FUTURE,
+      'date.order': DateError.DATE_ORDER,
     })
 }
---
+++ server/utils/errors.ts
@@ -0,0 +1,9 @@
+enum DateError {
+  DATE_REQUIRED = 'Date to and date from must both have a date',
+  DATE_FORMAT = 'Enter the date in the correct format, for example, 17/8/2025',
+  DATE_PAST = 'Dates must be on or after 1 August 2025',
+  DATE_FUTURE = 'Dates must not be in the future',
+  DATE_ORDER = 'Date to must be the same as or after date from',
+}
+
+export default DateError
