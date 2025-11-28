020ff140bee02a8f3eaf32ffb50619a1027caccf
Mary Ford <mary.ford@justice.gov.uk>
2025-11-28 10:27:23 +0000

    Update error messages

--- server/utils/errors.ts
+++ server/utils/errors.ts
@@ -3,7 +3,7 @@ enum DateError {
   DATE_FORMAT = 'Enter the date in the correct format, for example, 17/8/2025',
   DATE_PAST = 'Dates must be on or after 1 August 2025',
   DATE_FUTURE = 'Dates must not be in the future',
-  DATE_ORDER = 'Date to must be after date from',
+  DATE_ORDER = 'Date to must be the same as or after date from',
 }
 
 export default DateError
