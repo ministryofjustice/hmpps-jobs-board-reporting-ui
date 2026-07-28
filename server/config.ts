e3ff84f96abaf3dfcfb743e3d324b38d98848750
rickchoijd <rick.choi@justice.gov.uk>
2026-07-28 17:48:09 +0100

    Revert "ESWE-1927: Add Application Insights browser client (#70)"
    This reverts commit d15889a9aad58252d74139c54b25ae6fe3165476.

--- server/config.ts
+++ server/config.ts
@@ -142,7 +142,6 @@ export default {
   sqs: {
     audit: auditConfig(),
   },
-  appInsightsConnectionString: get('APPLICATIONINSIGHTS_CONNECTION_STRING', ''),
   domain: get('INGRESS_URL', 'http://localhost:3000', requiredInProduction),
   environmentName: get('ENVIRONMENT_NAME', ''),
   dpsHomeUrl: get('DPS_URL', 'http://localhost:3001/', requiredInProduction),
